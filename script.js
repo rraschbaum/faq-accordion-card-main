6e2ebc290af283863f0cc36cdcc1edebefa9b7a0
Ryan Raschbaum <ryanraschbaum@gmail.com>
2021-01-09 16:43:38 -0600

    updated accordian structure

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 let accordianItem = document.getElementsByClassName('accordian__item');
-let answerActive = document.getElementsByClassName('answer');
-let questionActive = document.getElementsByClassName('question__copy');
-let iconActive = document.getElementsByClassName('question__icon');
+let answerActive = document.getElementsByClassName('accordian__copy');
+let questionActive = document.getElementsByClassName('accordian__title');
+let iconActive = document.getElementsByClassName('accordian__icon');
 
 // Make a click event for each answer
 for (i = 0; i < accordianItem.length; i++) {
     let cur = i;
 
     accordianItem[i].addEventListener('click', function(){
-        answerActive[cur].classList.toggle('answer--active');
-        questionActive[cur].classList.toggle('question__copy--active');
-        iconActive[cur].classList.toggle('question__icon--active');
+        answerActive[cur].classList.toggle('accordian__copy--active');
+        questionActive[cur].classList.toggle('accordian__title--active');
+        iconActive[cur].classList.toggle('accordian__icon--active');
     }); 
 }
